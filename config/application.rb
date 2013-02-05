165b3fd123fd4259319cb3b7b631e3e73e8fab30
Fiona Strain <fiona.strain1@gmail.com>
2013-02-04 19:31:02 -0500

    removed index

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module Taskapp
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Eastern Time (US & Canada)'
 
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
@@ -55,8 +56,10 @@ module Taskapp
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
-    # Version of your assets, change this if you want to expire all your assets
+    config.assets.compile = true
+    #Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
 end
