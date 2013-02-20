0b841565ff6c8f029dffd13485b4cb61bc611687
Fiona Strain <fiona.strain1@gmail.com>
2013-02-19 21:42:01 -0500

    hi

--- db/migrate/20130214020701_create_documents.rb
+++ db/migrate/20130214020701_create_documents.rb
@@ -1,9 +1,9 @@
 class CreateDocuments < ActiveRecord::Migration
   def change
+    drop_table :documents
     create_table :documents do |t|
       t.string :name
       t.datetime :datemodified
-
       t.timestamps
     end
   end
