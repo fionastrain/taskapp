b89bc4da035d40c5af6ef52906946c3e4b822c3a
Fiona Strain <fiona.strain1@gmail.com>
2013-02-19 21:53:01 -0500

    removed files

--- db/migrate/20130204045429_create_documents.rb
+++
@@ -1,9 +0,0 @@
-class CreateDocuments < ActiveRecord::Migration
-  def change
-    create_table :documents do |t|
-      t.string :name
-      t.datetime :datemodified
-      t.timestamps
-    end
-  end
-end
